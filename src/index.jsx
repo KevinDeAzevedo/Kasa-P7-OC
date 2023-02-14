6a53608827ff2e3aa4f688b4cb5e61b14e50b7c4
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-02-14 18:25:38 +0100

    setup 404 page

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Error from './pages/Error';
 
 import './styles/main.scss';
 
@@ -13,8 +14,8 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/a-propos" element={<About />} />
-        {/* path * cible tous les endpoints inconnus et redirige sur Home */}
-        <Route path="*" element={<Home />} />
+        {/* path * cible tous les endpoints inconnus et redirige sur la page 404 */}
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
