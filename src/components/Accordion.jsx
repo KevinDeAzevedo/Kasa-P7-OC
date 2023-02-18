faef41bd6b68fa7bdced2e466e3f93c98bfa01c4
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-02-18 20:41:57 +0100

    Accordion style ok

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
+import ArrowBot from '../assets/arrow-bot.svg';
+import ArrowUp from '../assets/arrow-up.svg';
 
 export default function Accordion(props) {
   const { title, content } = props;
   const [isVisible, setVisible] = useState(false);
 
   return (
-    <div>
-      <div onClick={() => setVisible(!isVisible)}>
+    <div className="accordion">
+      <div onClick={() => setVisible(!isVisible)} className="accordion-title">
         <h3>{title}</h3>
-        <div>{isVisible ? '-' : '+'}</div>
+        <div>
+          {isVisible ? (
+            <img src={ArrowBot} alt="-" />
+          ) : (
+            <img src={ArrowUp} alt="+" />
+          )}
+        </div>
       </div>
-      {isVisible && <p>{content}</p>}
+      {isVisible && <p className="accordion-content">{content}</p>}
     </div>
   );
 }
