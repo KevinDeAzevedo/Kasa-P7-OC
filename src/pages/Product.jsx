8c646c4c367e49875b0d0e81086495ebf0203950
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-02-22 23:20:44 +0100

    Carousel primitive

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -9,6 +9,17 @@ export default function Product() {
   const [product, setProduct] = useState([]);
   const [equipments, setEquipments] = useState([]);
 
+  const array = ['One', 'Two', 'Three', 'Four']; // lenght = 3
+  const [index, setIndex] = useState(0);
+
+  function prevClick() {
+    return index === 0 ? setIndex(array.length - 1) : setIndex(index - 1);
+  }
+
+  function nextClick() {
+    return index >= array.length - 1 ? setIndex(0) : setIndex(index + 1);
+  }
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -32,6 +43,11 @@ export default function Product() {
         <div className="product-slider">Carrousel</div>
         <div className="product-header">
           <h1>{product.title}</h1>
+          <button onClick={prevClick}>Précédent</button>
+          <div>
+            {array[index]} {index + 1}/{array.length}
+          </div>
+          <button onClick={nextClick}>Suivant</button>
         </div>
         <div className="product-specs">Accordeons</div>
         {/* <ul>
