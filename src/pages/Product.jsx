a4a3b0094d98f8f52f8333ff0cdae231a709bf32
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-02-27 10:57:41 +0100

    product page data done

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Slideshow from '../components/Slideshow';
+import Accordion from '../components/Accordion';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
@@ -9,7 +10,17 @@ export default function Product() {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
   const [productPictures, setProductPictures] = useState([]);
-  const [equipments, setEquipments] = useState([]);
+  const [productTags, setProductTags] = useState([]);
+  const [productHost, setProductHost] = useState([]);
+  const [productDescription, setproductDescription] = useState();
+  const [productEquipments, setproductEquipments] = useState();
+  const stuff = (
+    <ul>
+      {productEquipments.map((equipment, index) => (
+        <li key={`${equipment}-${index}`}>{equipment}</li>
+      ))}
+    </ul>
+  );
 
   useEffect(() => {
     async function fetchData() {
@@ -18,8 +29,11 @@ export default function Product() {
         const data = await response.json();
         const foundProduct = data.find((product) => product.id === id);
         setProduct(foundProduct);
-        setEquipments(foundProduct.equipments);
+        setProductTags(foundProduct.tags);
+        setProductHost(foundProduct.host);
         setProductPictures(foundProduct.pictures);
+        setproductDescription(foundProduct.description);
+        setproductEquipments(foundProduct.equipments);
       } catch (error) {
         console.error(error);
         navigate('*');
@@ -33,32 +47,32 @@ export default function Product() {
       <Navigation />
       <div className="product">
         <div className="product-slideshow">
-          slider
           <Slideshow picture={productPictures} />
         </div>
         <div className="product-header">
           <div className="product-header-status">
             <h1>{product.title}</h1>
-            <p>Location</p>
+            <p>{product.location}</p>
             <div className="product-header-status-tags">
+              <ul>
+                {productTags.map((tag, index) => (
+                  <li key={`${tag}-${index}`}>{tag}</li>
+                ))}
+              </ul>
               <div>Paris</div>
               <div>Cozy</div>
             </div>
           </div>
           <div className="product-header-detail">
-            <p>Alexandre Dumas</p>
-            <div>Stars</div>
+            <img src={productHost.picture} alt="Portrait de l'hôte" />
+            <p>{productHost.name}</p>
+            <div>{product.rating}</div>
           </div>
         </div>
         <div className="product-specs">
-          <div>Accordeon n°1</div>
-          <div>Accordeon n°2</div>
+          <Accordion title="Description" content={productDescription} />
+          <Accordion title="Équipements" content={stuff} />
         </div>
-        {/* <ul>
-          {equipments.map((equipment, index) => (
-            <li key={`${equipment}-${index}`}>{equipment}</li>
-          ))}
-        </ul> */}
       </div>
       <Footer />
     </div>
