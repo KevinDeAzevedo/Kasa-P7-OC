5e86de285012ed50ba91ee9ab9ae4ee308c5b768
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-03-10 11:07:05 +0100

    refactoring

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,7 +4,9 @@ export default function HeroBanner(props) {
   const { title, image } = props;
   return (
     <section className="hero-banner">
-      <h1 className="hero-banner-title">{title}</h1>
+      {title === undefined ? null : (
+        <h1 className="hero-banner-title">{title}</h1>
+      )}
       <div className="hero-banner-image">
         <img src={image} alt="Bandeau d'illustration de la page" />
       </div>
